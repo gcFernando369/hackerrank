4acd2754eee74062420bbc3c89b44974e3c131f8
Fernando González <gc.fernando.96@gmail.com>
2022-04-28 11:19:34 -0500

    one way

--- oneWay.py
+++ oneWay.py
@@ -1,32 +1,28 @@
-def oneWay(a:str, b:str) -> bool:
-  len_a = len(a)
-  len_b = len(b)
+def oneWay(s1:str, s2:str) -> bool:
+  len_s1, len_s2 = len(s1), len(s2)
+  if abs(len_s1 - len_s2) > 1: return False
 
-  if abs(len_a - len_b) > 1: return False
+  if len_s1 >= len_s2: a, b = s1, s2
+  else: a, b = s2, s1
 
-  if len_a >= len_b:
-    s1 = a
-    s2 = b
-  else:
-    s1 = b
-    s2 = a
-
+  i, j = 0, 0
   diffs = 0
-  j = 0
-  i = 0
+  len_a, len_b = len(a), len(b)
 
-  while i < len(s1) and j < len(s2):
-
-    if s1[i] == s2[j]:
+  while i < len_a and j < len_b:    
+    if a[i] == b[j]:
       i += 1
       j += 1
     else:
-      i += 1
       diffs += 1
-
-    if i+1 < len(s1) and j+1 < len(s2) and s1[i+1] == s2[j+1]:
-      i += 1
-      j += 1
+      next_i = i + 1
+      next_j = j + 1
+      if next_i < len_a and a[next_i] == b[j]:
+        i = next_i
+      elif next_i < len_a and next_j < len_b and a[next_i] == b[next_j]:
+        i = next_i
+        j = next_j
+      else: break
     
   is_valid = diffs < 2
   return is_valid
@@ -35,3 +31,4 @@ print(oneWay('pale', 'ple'))
 print(oneWay('pales', 'pale'))
 print(oneWay('pale', 'bale'))
 print(oneWay('pale', 'bake'))
+print(oneWay('pale', 'pala'))
